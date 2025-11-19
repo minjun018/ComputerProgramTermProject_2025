68a42b8ec533651d81834cef3781e5aa24166eea
minjun018 <eptwl@daum.net>
2025-11-19 20:02:58 +0900

    account 속성 수정

--- src/Account.java
+++ src/Account.java
@@ -3,12 +3,14 @@ public class Account {
     String id;
     String pw;
     boolean manage;//학생인지 과사무실분들인지 구분하는 인자
+    int uni_number;
     //관리자면 true 사용자면 false
     public static List<Account> member = new ArrayList<>();
-    public Account(String id,String pw,boolean manage){//Account 생성자
+    public Account(String id,String pw,boolean manage,int uni_number){//Account 생성자
         this.id = id;
         this.pw = pw;
         this.manage = manage;
+        this.uni_number = uni_number;
     }
     public static void Register(Account person){//유저등록 메소드
         member.add(person);
